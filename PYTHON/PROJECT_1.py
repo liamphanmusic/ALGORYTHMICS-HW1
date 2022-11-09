6d8dfecf98bb35a7d80382a4a8661da351a74cc9
LiamPhan17 <liam.phan2000@gmail.com>
2022-11-09 14:05:11 +0100

    2

--- PYTHON/PROJECT_1.py
+++ PYTHON/PROJECT_1.py
@@ -3,7 +3,7 @@ import re
 def read_reference_text(filename: str) -> list[list[str]]:
     f = open(filename)
     line = f.readlines()
-    line = [re.split("[ ,;:\’\"\?!.]+", l) for l in line]
+    line = [re.split("[ ,;:\’\"\?!.']+", l) for l in line]
     line = [line[:-1] for line in line]
     line = [[line.lower() for line in line] for line in line]
     f.close()
@@ -11,10 +11,28 @@ def read_reference_text(filename: str) -> list[list[str]]:
 
 
 def make_word_vector(w: str, txt: list[list[str]]) -> dict[str, int]:
-
+    dict = {}
+    for sentence in txt:
+        if w in sentence:
+            sentence.pop(sentence.index(w))
+            temporary = set(sentence)
+            for word in temporary:
+                if word in dict.keys():
+                    dict[word] = dict[word] + 1
+                else:
+                    dict[word] = 1
+    return dict
 
 w = ["spain", "anchovy","france", "internet", "china", "mexico", "fish", "industry", "agriculture", "fishery", "tuna", "transport", "italy", "web", "communication", "labour", "fish", "cod"]
-txt = read_reference_text("ref-sentences.txt")
 
-print(make_word_vector(w,txt))
+txt = read_reference_text("ref-sentences.txt")
+dict = make_word_vector(w,txt)
+print(dict)
+
+
+
+
+
+
+
 
